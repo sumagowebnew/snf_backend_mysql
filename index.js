d693ccd8f842d1db83b5d5da7e0b5d71cd93d5ce
Rajendra Fodase <rajendrafodase61@gmail.com>
2024-06-12 13:05:09 +0530

    changes in route

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 // Routes
 try {
-  
   const articleRoute = require("./src/routes/articleRoute")
   app.use("/article_on_snf", articleRoute)
 
